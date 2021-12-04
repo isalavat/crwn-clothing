f2759f5cc8bbcfc6010cce0429996b9e63c79f2d
Salavat <salavat.islamov.96@gmail.com>
2021-12-04 08:44:27 +0100

    homepage was splitted into smaller components and menu-item styled

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./homepage.styles.scss";
+import Directory from "../../components/directory/directory.component"; 
+const HomePage = () => (
+    <Directory></Directory>
+);
+
+export default HomePage;
