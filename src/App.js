7466e7a258aa8fa108912f7a42260f4706fe88f7
Salavat <salavat.islamov.96@gmail.com>
2021-12-05 08:31:53 +0100

    Routing added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import './App.css';
+import {  Route, Routes } from "react-router-dom";
 
+import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-function App() {
-  return <div>
-    <HomePage/>
+
+
+const HatsPage  = () => (
+  <div>
+    <h1>HATS PAGE</h1>
   </div>
+);
+
+function App() {
+  return (<div>
+    <Routes>
+      <Route exact path="/" element={<HomePage/>}/>
+      <Route  path="/hats" element={<HatsPage/>}/>
+    </Routes>
+  </div>);
 }
 
 export default App;
