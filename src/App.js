8f4532d8ccc89a1309070fa947fc4acac04c9b93
Salavat <salavat.islamov.96@gmail.com>
2021-12-11 18:44:04 +0100

    Custom FormInput component added

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import {  Route, Routes } from "react-router-dom";
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component.jsx';
-
+import Header from './components/header/header.component';
+import SingInAndSignUp from './pages/sign-in-and-sign-up/sign-in-an-sign-up.component';
 function App() {
   return (<div>
     <Header/>
     <Routes>
       <Route exact path="/" element={<HomePage/>}/>
       <Route  path="/shop" element={<ShopPage/>}/>
+      <Route  path="/signin" element={<SingInAndSignUp/>}/>
     </Routes>
   </div>);
 }
